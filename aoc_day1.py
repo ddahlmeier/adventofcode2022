c16600262951e6eeb83061590982c0dfb976e716
Daniel Dahlmeier <d.dahlmeier@sap.com>
2022-12-13 15:21:06 +0800

    python solution day 1 part 2

--- aoc_day1.py
+++ aoc_day1.py
@@ -13,4 +13,9 @@ inputs = """1000
 
 10000"""
 
+# part 1: find the maximum amount of calories carried by any elve
 max([sum(map(lambda x:int(x), block.split())) for block in inputs.strip().split("\n\n")])
+
+# part 2: find the amount of calories carried by the top 3 elves
+sum(sorted([sum(map(lambda x:int(x), block.split())) for block in inputs.strip().split("\n\n")], reverse=True)[:3])
+
