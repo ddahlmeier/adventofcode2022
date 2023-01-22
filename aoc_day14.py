39325d2b418685f24009a72a83e77ecc87ab3b61
Daniel Dahlmeier <d.dahlmeier@sap.com>
2023-01-22 21:02:19 +0800

    solution day 14 part 2

--- aoc_day14.py
+++ aoc_day14.py
@@ -1,5 +1,7 @@
 # Advent of Code 14: Regolith Reservoir
 
+from operator import itemgetter
+
 inputs = """498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9""".splitlines()
 
@@ -41,28 +43,34 @@ def print_map(rocks, sands, entrance, rangex, rangey):
             print(symbol, end="")
         print("")
     
-def isblocked(x, y, rocks, sands):
+def isblocked(x, y, rocks, sands,  bottom=None):
+    if bottom and y == bottom:
+        return True
     return (x, y) in rocks or (x, y) in sands
 
-def simulate_step(sand_unit, rocks, sands):
+def simulate_step(sand_unit, rocks, sands, bottom=None):
     x, y = sand_unit
-    if not isblocked(x, y+1, rocks, sands):
+    if not isblocked(x, y+1, rocks, sands, bottom):
         return (x, y+1), False
-    elif not isblocked(x-1, y+1, rocks, sands):
+    elif not isblocked(x-1, y+1, rocks, sands, bottom):
         return (x-1, y+1), False
-    elif not isblocked(x+1, y+1, rocks, sands):
+    elif not isblocked(x+1, y+1, rocks, sands, bottom):
         return (x+1, y+1), False
     else:
         return (x, y), True
 
-def simulate(sand, rocks, sands, max_depth):
+def simulate(sand, rocks, sands, max_depth, bottom=None):
     blocked = False
     while not blocked:
-        sand, blocked = simulate_step(sand, rocks, sands)
+        sand, blocked = simulate_step(sand, rocks, sands, bottom)
         if blocked:
             sands.append(sand)
-            return sands, False
-        elif sand[1] >= max_depth:
+            if bottom and sand == (500,0):
+                print("entrance is clocked")
+                return sands, True
+            else:
+                return sands, False
+        elif bottom == None and sand[1] >= max_depth:
             print("sand falls out the bottom")
             return sands, True
     return sands
@@ -75,10 +83,21 @@ max_depth = max(map(itemgetter(1), rocks))+1
 for i in range(1000):
     print("simulate sand unit:", i)
     sands, completed = simulate((500, 0), rocks, sands, max_depth)
-    if i % 100 == 0:
-        print_map(rocks, sands, (500,0), (420,550), (0,170))
+    if i % 2 == 0:
+        print_map(rocks, sands, (500,0), (420,550), (0,12))
     if completed:
         break
 
+# part 2
+sands = []
+rocks = parse(inputs)
+max_depth = max(map(itemgetter(1), rocks))+2
+for i in range(100000):
+    sands, completed = simulate((500, 0), rocks, sands, max_depth, bottom=max_depth)
+    if i % 1000 == 0:
+        print("simulate sand unit:", i)
+        print_map(rocks, sands, (500,0), (420,550), (0,170))
+    if completed:
+        break
 
 
